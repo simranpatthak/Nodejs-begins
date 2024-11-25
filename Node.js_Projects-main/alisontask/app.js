677e289bee1c068d26d36cd64eb486764fcad42b
simranpatthak <suman.patthak1166@gmail.com>
2024-11-25 11:31:28 +0530

    upload

---
+++ Node.js_Projects-main/alisontask/app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const fs = require("fs");
+
+function req(request, response) {
+  fs.readFile("index.html", function (err, data) {
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+  });
+  response.write(data);
+  response.end();
+}
+http.createServer(req).listen(4545);
+console.log("Server running");
